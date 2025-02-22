fb65bee811510a4269c73906ccce244af84549ec
Clifton <yeoclifton@gmail.com>
2025-02-22 20:30:59 +0800

    Upload base files

---
+++ namelist-web-app/tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: '#7CB9E8',
+        danger: '#EF4444',
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
